b0c81d6091e8398360f9c7e71674cfd2daefe4dd
hakier120 <damian.wolski9998@wp.pl>
2022-09-14 19:58:34 +0100

    No changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {EmployeeServiceModule} from "./services/employee.service-module";
 import {ServiceArchServiceModule} from "./services/service-arch.service-module";
 import {ProjectServiceModule} from "./services/project.service-module";
 import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
+import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +23,8 @@ import {ProjectListComponentModule} from "./ui/project-list/project-list.compone
         EmployeeFacesComponentModule,
         ProjectServiceModule,
         ProjectListComponentModule,
+        EmployeeFormComponentModule,
+
     ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/ui/employee-form/employee-form.component-module.ts
+++ src/app/ui/employee-form/employee-form.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeFormComponent } from './employee-form.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [EmployeeFormComponent],
   providers: [],
   exports: [EmployeeFormComponent]
