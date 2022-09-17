c5a65446390ff4f64c819ed6b5570e11e0aa836a
hakier120 <damian.wolski9998@wp.pl>
2022-09-17 17:51:24 +0100

    Exercise part 2 2.1 Dodanie alertu

--- src/app/ui/userform/userform.component.ts
+++ src/app/ui/userform/userform.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-userform',
@@ -13,4 +13,8 @@ export class UserformComponent {
     age: new FormControl(),
     salary: new FormControl()
   });
+
+  onButtonclicked() {
+    alert('User was successfully added to the database')
+  }
 }
