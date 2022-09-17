fe4c3be2d233945d4029046d8865bbf67a04c949
hakier120 <damian.wolski9998@wp.pl>
2022-09-17 17:53:51 +0100

    Exercise part 2 2.1 Dodanie userform do modułu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,37 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EmployeeListComponentModule} from "./ui/employee-list/employee-list.component-module";
 import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
 import {EmployeeServiceModule} from "./services/employee.service-module";
 import {ServiceArchServiceModule} from "./services/service-arch.service-module";
 import {ProjectServiceModule} from "./services/project.service-module";
 import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
 import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
+import {UserformComponent} from "./ui/userform/userform.component";
+import {UserformComponentModule} from "./ui/userform/userform.component-module";
 
 @NgModule({
   declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        EmployeeListComponentModule,
-        EmployeeServiceModule,
-        ServiceArchServiceModule,
-        EmployeeFacesComponentModule,
-        ProjectServiceModule,
-        ProjectListComponentModule,
-        EmployeeFormComponentModule,
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    EmployeeListComponentModule,
+    EmployeeServiceModule,
+    ServiceArchServiceModule,
+    EmployeeFacesComponentModule,
+    ProjectServiceModule,
+    ProjectListComponentModule,
+    EmployeeFormComponentModule,
+    UserformComponentModule,
 
-    ],
+
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
