2b91688d672b4858fc865995bd6e93bdfbf85c72
hakier120 <damian.wolski9998@wp.pl>
2022-09-18 23:05:44 +0100

    Part 2 Create Operation task Zmiana docelowego url oraz drobne poprawki

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
-import {FormControl, FormGroup, Validator, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {EmployeeService} from '../../services/employee.service';
 import {CreateEmployeeModel} from '../../model/create-employee.model';
 
@@ -13,18 +13,16 @@ export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
     age: new FormControl(null, [Validators.min(0), Validators.required]),
-    salary: new FormControl(null, [Validators.min(0), Validators.minLength(2)]),
+    salary: new FormControl(null, [Validators.required, Validators.minLength(2)]),
 
   });
 
   constructor(private _employeeService: EmployeeService) {
   }
 
-  onFormsubmitted(form: CreateEmployeeModel) {
+  onFormSubmitted(form: CreateEmployeeModel) {
     this._employeeService.Create({name: form.name, age: form.age, salary: form.salary}).subscribe();
 
-
   }
-
 
 }
