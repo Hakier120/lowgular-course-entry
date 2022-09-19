88b2cf71286ebdd0b034db403878327c48f8c039
hakier120 <damian.wolski9998@wp.pl>
2022-09-19 20:25:00 +0100

    Part 2 task 4.1 Zapisanie metody delete

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {PersonModel} from '../model/person.model';
-import {CreateEmployeeModel} from '../model/create-employee.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PersonModel } from '../model/person.model';
+import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -17,6 +17,9 @@ export class EmployeeService {
   Create(employee: CreateEmployeeModel): Observable<void> {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(_ => void 0));
 
+  }
+
+  delete(id: string): Observable<void> {
   }
 }
 
