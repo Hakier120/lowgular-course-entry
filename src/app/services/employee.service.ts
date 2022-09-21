fa45a05bcbd860d64bb0c044bfd6be055fb0f9c3
hakier120 <damian.wolski9998@wp.pl>
2022-09-21 20:27:56 +0100

    Part 2 task 5 Dodanie ApiResponse<EmployeeResponse> Niestety nie znajduje mi 'EmployeeResponse', świeci się na czerwono i nie działa przez to kod. Tak samo nie znajduje profile_image, employee_name

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,15 +4,25 @@ import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {PersonModel} from '../model/person.model';
 import {CreateEmployeeModel} from '../model/create-employee.model';
+import {ApiResponse} from "./api.response";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
-
+   
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get(undefined,).pipe(
-      map((arrayBuffer: ArrayBuffer): PersonModel[] => undefined));
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/').pipe(
+      map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            mail: employeeResponse.employee_name + "@lowgular.io",
+            img: employeeResponse.profile_image,
+            personalNumber: employeeResponse.id
+          }
+        })
+      }));
 
   }
 
