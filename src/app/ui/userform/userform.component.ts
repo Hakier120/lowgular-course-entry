3f42dfed5afd425c10132c9d5efbf650d81fb9a7
hakier120 <damian.wolski9998@wp.pl>
2022-09-26 20:25:29 +0100

    Part 2 final exam Dodanie komponentu / Dodanie formy oraz alertu po wciśnięciu submit

---
+++ src/app/ui/userform/userform.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {EmployeeService} from '../../services/employee.service';
+
+@Component({
+  selector: 'app-userform',
+  templateUrl: './userform.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserformComponent {
+  readonly userForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    age: new FormControl(),
+    email: new FormControl()
+  });
+
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  OnButtonClicked(form: { email: string, age: string, name: string }) {
+    alert('User was succesfully added to the database.' + 'Email: ' + form.email + 'Age' + form.age + 'Name' + form.name)
+  }
+}
