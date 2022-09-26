5450f22122b38c0d3fce467e44143e499ca94f30
hakier120 <damian.wolski9998@wp.pl>
2022-09-26 11:51:07 +0100

    Part 2 final exam Stworzenie modelu, componentu oraz serwisu

---
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeeListComponent {
+}
