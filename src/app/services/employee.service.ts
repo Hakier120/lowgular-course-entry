78c7a5eb5b7cd54c14c6c99bf70d3cfa34fb1050
hakier120 <damian.wolski9998@wp.pl>
2022-09-26 12:51:47 +0100

    Part 2 final exam Dodanie interfejsu employeeresponse

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface EmployeeResponse {
+  id: string;
+  employee_name: string;
+  employee_salary: string;
+  employee_age: string;
+  profile_image: string;
+}
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
+
+
 }
+
