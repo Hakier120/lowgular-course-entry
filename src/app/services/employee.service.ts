60581f69a321e0c2929d5e78ce1ea04d6fd06d5e
hakier120 <damian.wolski9998@wp.pl>
2022-09-26 19:42:40 +0100

    Part 2 final exam Przemapowanie oraz dodanie pobierania pracowników z dummyrestapi

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,5 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map, Observable} from "rxjs";
+import {PersonModel} from "../model/person.model";
+import {ApiResponse} from "./api.response";
+
 
 export interface EmployeeResponse {
   id: string;
@@ -14,6 +18,21 @@ export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
 
+  getAll(): Observable<PersonModel[]> {
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(
+      map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            mail: employeeResponse.employee_name,
+            img: employeeResponse.profile_image,
+            personalNumber: employeeResponse.id,
+            id: employeeResponse.id,
+          }
+        })
+      })
+    )
+  }
 
 }
 
