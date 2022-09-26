0f4563a00fff1dc468b7026f0cd45e524266fc61
hakier120 <damian.wolski9998@wp.pl>
2022-09-26 20:26:09 +0100

    Part 2 final exam Dodanie userform module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {EmployeeListComponentModule} from "./ui/employee-list/employee-list.component-module";
-import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
+
 import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ServiceArchServiceModule} from "./services/service-arch.service-module";
-import {ProjectServiceModule} from "./services/project.service-module";
-import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
-import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
-import {UserformComponent} from "./ui/userform/userform.component";
 import {UserformComponentModule} from "./ui/userform/userform.component-module";
 
 @NgModule({
@@ -19,7 +14,10 @@ import {UserformComponentModule} from "./ui/userform/userform.component-module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    
+    EmployeeListComponentModule,
+    EmployeeServiceModule,
+    UserformComponentModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
