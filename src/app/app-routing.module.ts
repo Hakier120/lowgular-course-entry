9085f9f3bf4f235c9932784a7d0c26ca8b00fab1
hakier120 <damian.wolski9998@wp.pl>
2022-10-14 19:09:12 +0100

    Reconstruct cozz of an error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
-import { UserformComponent } from './ui/userform/userform.component';
-import { HomeComponent } from './ui/home/home.component';
-import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
-import { EmployeeServiceModule } from './services/employee.service-module';
-import { UserformComponentModule } from './ui/userform/userform.component-module';
-import { HomeComponentModule } from './ui/home/home.component-module';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {EmployeeListComponent} from './ui/employee-list/employee-list.component';
+import {UserformComponent} from './ui/userform/userform.component';
+import {EmployeeListComponentModule} from './ui/employee-list/employee-list.component-module';
+import {EmployeeServiceModule} from './services/employee.service-module';
+import {UserformComponentModule} from './ui/userform/userform.component-module';
 
-const routes: Routes = [{ path: 'employee-list', component: EmployeeListComponent }, { path: 'create-employee', component: UserformComponent }, { path: '', component: HomeComponent }];
+
+const routes: Routes = [{path: 'employee-list', component: EmployeeListComponent}, {
+  path: 'create-employee', component: UserformComponent
+}]
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule, HomeComponentModule],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
