2f71a1425bf98c9e08fdf97c0947c61a3fda41db
hakier120 <damian.wolski9998@wp.pl>
2022-10-14 18:37:50 +0100

    Part 4 app routing and details  1.3  Subtask 2  Employee list connected with path employee-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
-import { UserformComponent } from './ui/userform/userform.component';
-import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
-import { EmployeeServiceModule } from './services/employee.service-module';
-import { UserformComponentModule } from './ui/userform/userform.component-module';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {EmployeeListComponent} from './ui/employee-list/employee-list.component';
+import {UserformComponent} from './ui/userform/userform.component';
+import {EmployeeListComponentModule} from './ui/employee-list/employee-list.component-module';
+import {EmployeeServiceModule} from './services/employee.service-module';
+import {UserformComponentModule} from './ui/userform/userform.component-module';
 
-const routes: Routes = [{ path: '', component: EmployeeListComponent }, { path: 'create-employee', component: UserformComponent }];
+const routes: Routes = [{path: 'employee-list', component: EmployeeListComponent}, {
+  path: 'create-employee',
+  component: UserformComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
