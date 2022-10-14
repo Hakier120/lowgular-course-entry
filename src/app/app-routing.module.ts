10d37e58cd012ea4f5e9f6a578d724ad15297a3c
hakier120 <damian.wolski9998@wp.pl>
2022-10-14 19:11:05 +0100

    part4 task 1.3 subtask 3 Home route with path '' added once again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,20 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeeListComponent} from './ui/employee-list/employee-list.component';
 import {UserformComponent} from './ui/userform/userform.component';
+import {HomeComponent} from './ui/home/home.component';
 import {EmployeeListComponentModule} from './ui/employee-list/employee-list.component-module';
 import {EmployeeServiceModule} from './services/employee.service-module';
 import {UserformComponentModule} from './ui/userform/userform.component-module';
-
+import {HomeComponentModule} from './ui/home/home.component-module';
 
 const routes: Routes = [{path: 'employee-list', component: EmployeeListComponent}, {
-  path: 'create-employee', component: UserformComponent
-}]
+  path: 'create-employee',
+  component: UserformComponent
+}, {path: '', component: HomeComponent}]
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule, HomeComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
