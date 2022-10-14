0870e31f6eedbec4fdebcbfb6fcaa0e4913fee4c
hakier120 <damian.wolski9998@wp.pl>
2022-10-14 18:25:43 +0100

    Part 4 app routing and details Dodanie ścieżek i obslugiwanie przez nie komponenty przez lowgular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { UserformComponent } from './ui/userform/userform.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { UserformComponentModule } from './ui/userform/userform.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EmployeeListComponent }, { path: 'create-employee', component: UserformComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
