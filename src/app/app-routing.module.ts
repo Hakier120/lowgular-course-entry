e8ba1e7f582e8b13e24045797a0fc20836ecce6a
hakier120 <damian.wolski9998@wp.pl>
2022-10-17 23:42:00 +0100

    Part 4. exercise 3. Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {EmployeeListComponent} from './ui/employee-list/employee-list.component';
-import {UserformComponent} from './ui/userform/userform.component';
-import {HomeComponent} from './ui/home/home.component';
-import {EmployeeListComponentModule} from './ui/employee-list/employee-list.component-module';
-import {EmployeeServiceModule} from './services/employee.service-module';
-import {UserformComponentModule} from './ui/userform/userform.component-module';
-import {HomeComponentModule} from './ui/home/home.component-module';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { UserformComponent } from './ui/userform/userform.component';
+import { HomeComponent } from './ui/home/home.component';
+import { EmployeeDetailsComponent } from './ui/employee-details/employee-details.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { UserformComponentModule } from './ui/userform/userform.component-module';
+import { HomeComponentModule } from './ui/home/home.component-module';
+import { EmployeeDetailsComponentModule } from './ui/employee-details/employee-details.component-module';
 
-const routes: Routes = [{path: 'employee-list', component: EmployeeListComponent}, {
-  path: 'create-employee',
-  component: UserformComponent
-}, {path: '', component: HomeComponent}]
+const routes: Routes = [{ path: 'employee-list', component: EmployeeListComponent }, { path: 'create-employee', component: UserformComponent }, { path: '', component: HomeComponent }, { path: 'employee-details/:id', component: EmployeeDetailsComponent }]
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule, HomeComponentModule],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, UserformComponentModule, HomeComponentModule, EmployeeDetailsComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
