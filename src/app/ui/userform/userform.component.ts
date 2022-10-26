aa5e5a2ff2de72365b128373d537ad0d0ff0212b
hakier120 <damian.wolski9998@wp.pl>
2022-10-26 12:20:07 +0100

    Part 4 Exercise 4.3 Added validators and communication after success

--- src/app/ui/userform/userform.component.ts
+++ src/app/ui/userform/userform.component.ts
@@ -1,6 +1,8 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {EmployeeService} from '../../services/employee.service';
+import {EmployeeModel} from "../../model/employee.model";
+import {CreateEmployeeModel} from "../../model/create-employee.model";
 
 @Component({
   selector: 'app-userform',
@@ -10,15 +12,26 @@ import {EmployeeService} from '../../services/employee.service';
 })
 export class UserformComponent {
   readonly userForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    age: new FormControl(),
-    email: new FormControl()
+    name: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
+    salary: new FormControl(null, [Validators.required])
   });
 
   constructor(private _employeeService: EmployeeService) {
   }
 
-  OnButtonClicked(form: { email: string, age: string, name: string }) {
-    alert('User was succesfully added to the database.' + ' Email: ' + form.email + 'Age' + form.age + 'Name' + form.name)
+  OnButtonClicked(form: CreateEmployeeModel): void {
+    this._employeeService.Create({id: form.id, name: form.name, mail: form.mail, age: form.age}).subscribe(
+      ({
+        complete() {
+          alert('User was succesfully added to the system!')
+        }
+      })
+    );
+
+
   }
+
+
 }
